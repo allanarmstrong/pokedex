0222a084473f15f41309a878ad74d9be9c742b4e
allanarmstrong <allan@hit3k.net>
2020-01-16 20:34:49 +1100

    Add Reach Router

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
+
 import Pokedex from "./Pokedex";
 
 window.CONSTANTS = {
   API_URL: `https://pokeapi.co/api/v2`,
-  POKEMON_SPRITES: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon`
+  POKEMON_SPRITES: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon`,
+  POKEMON_COUNT: 807
 };
 
-ReactDOM.render(<Pokedex />, document.getElementById("app"));
+ReactDOM.render(
+  <Router>
+    <Pokedex path="/" />
+  </Router>,
+  document.getElementById("app")
+);
